66fa31ab9841c37c90b37c183275db6b0bbb2307
maaz-afridi20 <maazafridi.ma47@gmail.com>
2023-03-10 12:12:59 +0500

    connecting with firebase..

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:smart_blood_bank/consts/colors.dart';
 import 'package:smart_blood_bank/views/create_account.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
